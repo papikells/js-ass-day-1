c5cb9bc92f4420291eb3bed183e1d0089ebfb3ab
papikells <105240686+papikells@users.noreply.github.com>
2023-07-26 18:07:45 +0100

    Add files via upload

---
+++ .js
@@ -0,0 +1,114 @@
+// Comments can make code readable
+
+// Welcome to 30DaysOfJavaScript
+
+/*
+Comments can make code readable,
+easy to reuse, and informative.
+*/
+
+
+// variable.js
+
+// String variable
+const name = "Ibukun Doe";
+
+// Boolean variable
+const isStudent = true;
+
+// Undefined variable
+let age;
+
+// Null variable
+const favoriteColor = null;
+
+
+
+
+
+// datatypes.js
+
+// String variable
+const name = "Ibukun jon";
+console.log("Data type of name:", typeof name);
+
+// Boolean variable
+const isStudent = true;
+console.log("Data type of isStudent:", typeof isStudent);
+
+// Number variable
+const age = 25;
+console.log("Data type of age:", typeof age);
+
+// Undefined variable
+let job;
+console.log("Data type of job:", typeof job);
+
+// Null variable
+const favoriteColor = null;
+console.log("Data type of favoriteColor:", typeof favoriteColor);
+
+// Array variable
+const hobbies = ["reading", "gaming", "cooking"];
+console.log("Data type of hobbies:", typeof hobbies);
+
+// Object variable
+const person = { name: "Alice", age: 30, isStudent: false };
+console.log("Data type of person:", typeof person);
+
+// Function variable
+function greet() {
+  console.log("Hello!");
+}
+console.log("Data type of greet:", typeof greet);
+
+// Symbol variable
+const id = Symbol("id");
+console.log("Data type of id:", typeof id);
+
+
+
+// Using var
+var variable1;
+var variable2;
+
+// Using let
+let variable3;
+let variable4;
+
+// Using const
+const variable5;
+const variable6;
+
+
+
+// Using var
+var variable1 = "Hello";
+var variable2 = 42;
+
+// Using let
+let variable3 = false;
+let variable4 = [1, 2, 3];
+
+// Using const
+const variable5 = "Welcome";
+const variable6 = { name: "John", age: 30 };
+
+
+let firstName = "ibukun";
+let lastName = "Doe";
+let maritalStatus = "single";
+let country = "United States";
+let age = 30;
+
+
+let firstName = "ibukun", lastName = "Doe", maritalStatus = "single", country = "United States", age = 30;
+
+
+// Declare and assign initial values to variables
+let myAge = 25;
+let yourAge = 30;
+
+// Log the values to the console
+console.log("My age:", myAge);
+console.log("Your age:", yourAge);
